a93599cc91d9b1921728c4843857762a37fd2d9f
karabink <48476624+karabink@users.noreply.github.com>
2020-10-16 22:03:14 +0300

    Created a new project

---
+++ Assets/Plita.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plita : MonoBehaviour
+{
+    public float Speed = 0.5f;
+
+    public Vector3 Direction;
+
+    // Start is called before the first frame update
+
+    void Start()
+
+    {
+
+        Direction = transform.right;
+
+    }
+
+    // Update is called once per frame
+
+    void Update()
+
+    {
+
+        transform.Translate(Direction * Speed * Time.deltaTime);
+
+    }
+}
